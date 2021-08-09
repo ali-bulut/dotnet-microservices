39ddd6eb72494b73178f094af7d4ad75df59308e
Ali Bulut <ali@alibulut.net>
2021-08-09 16:41:12 +0300

    creating CustomBaseController

---
+++ Shared/FreeCourse.Shared/ControllerBase/CustomBaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Shared.ControllerBase
+{
+    public class CustomBaseController : Controller
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
