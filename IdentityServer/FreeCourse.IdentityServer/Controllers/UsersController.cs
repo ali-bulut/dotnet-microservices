11fff123a91189c653ab48803a06a8fa2089958b
Ali Bulut <ali@alibulut.net>
2021-08-10 16:39:43 +0300

    implementing GetUser endpoint to IdentityServer.API

--- IdentityServer/FreeCourse.IdentityServer/Controllers/UsersController.cs
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 using FreeCourse.IdentityServer.Dtos;
@@ -35,6 +36,25 @@ namespace FreeCourse.IdentityServer.Controllers
             }
 
             return NoContent();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Sub);
+            if(userIdClaim == null)
+            {
+                return BadRequest();
+            }
+
+            var user = await _userManager.FindByIdAsync(userIdClaim.Value);
+
+            if(user == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(new { Id = user.Id, UserName = user.UserName, Email = user.Email, City = user.City });
         }
     }
 }
