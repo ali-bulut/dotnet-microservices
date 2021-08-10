4c2093919f0a2afc1571ed4a329b669e283d4d4a
Ali Bulut <ali@alibulut.net>
2021-08-10 14:57:29 +0300

    generating migrations

---
+++ IdentityServer/FreeCourse.IdentityServer/Migrations/20210810115210_remove_test_migration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FreeCourse.IdentityServer.Migrations
+{
+    public partial class remove_test_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TestField",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TestField",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
