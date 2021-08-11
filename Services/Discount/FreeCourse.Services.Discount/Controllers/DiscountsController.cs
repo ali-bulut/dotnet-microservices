c2884198d877cb00f741e7d0369f376f391bb99d
Ali Bulut <ali@alibulut.net>
2021-08-11 17:55:06 +0300

    bugfix: [FromBody] added to endpoints

--- Services/Discount/FreeCourse.Services.Discount/Controllers/DiscountsController.cs
+++ Services/Discount/FreeCourse.Services.Discount/Controllers/DiscountsController.cs
@@ -42,13 +42,13 @@ namespace FreeCourse.Services.Discount.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Save(Models.Discount discount)
+        public async Task<IActionResult> Save([FromBody] Models.Discount discount)
         {
             return CreateActionResultInstance(await _discountService.Save(discount));
         }
 
         [HttpPut]
-        public async Task<IActionResult> Update(Models.Discount discount)
+        public async Task<IActionResult> Update([FromBody] Models.Discount discount)
         {
             return CreateActionResultInstance(await _discountService.Update(discount));
         }
