ee68650651b0c86fd11b38e29e58c97a75633b32
Ali Bulut <ali@alibulut.net>
2021-08-12 17:53:30 +0300

    implementing Command and CommandHandler by using MediatR library

---
+++ Services/Order/FreeCourse.Services.Order.Application/Commands/CreateOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using FreeCourse.Services.Order.Application.Dtos;
+using FreeCourse.Shared.Dtos;
+using MediatR;
+
+namespace FreeCourse.Services.Order.Application.Commands
+{
+    public class CreateOrderCommand : IRequest<Response<CreatedOrderDto>>
+    {
+        public string CustomerId { get; set; }
+        public List<OrderItemDto> OrderItems { get; set; }
+        public AddressDto Address { get; set; }
+    }
+}
