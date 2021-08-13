cc6fe6e3f4c03a2ac3cdc870e75f356efe763b70
Ali Bulut <ali@alibulut.net>
2021-08-13 14:58:31 +0300

    creating FakePaymentsController

---
+++ Services/FakePayment/FreeCourse.Services.FakePayment/Controllers/FakePaymentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreeCourse.Shared.ControllerBase;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.FakePayment.Controllers
+{
+    [Route("api/[controller]")]
+    public class FakePaymentsController : CustomBaseController
+    {
+       [HttpPost]
+       public IActionResult ReceivePayment()
+       {
+           return CreateActionResultInstance(Response<NoContent>.Success(200));
+       }
+    }
+}
