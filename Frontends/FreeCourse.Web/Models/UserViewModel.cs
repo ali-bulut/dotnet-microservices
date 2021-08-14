2764585159c5ed306d967d3698ef1d4fa9d5cd36
Ali Bulut <ali@alibulut.net>
2021-08-14 15:41:41 +0300

    creating IUserService and UserViewModel

---
+++ Frontends/FreeCourse.Web/Models/UserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FreeCourse.Web.Models
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+
+        public IEnumerable<string> GetUserProps()
+        {
+            yield return Username;
+            yield return Email;
+            yield return City;
+        }
+    }
+}
