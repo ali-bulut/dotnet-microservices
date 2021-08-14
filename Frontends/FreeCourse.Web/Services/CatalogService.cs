dca12073f5d96177d6187b9025bb9c4453c9e9a1
Ali Bulut <ali@alibulut.net>
2021-08-14 17:38:29 +0300

    configuring CatalogService settings

---
+++ Frontends/FreeCourse.Web/Services/CatalogService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FreeCourse.Web.Models;
+using FreeCourse.Web.Models.Catalog;
+using FreeCourse.Web.Services.Interfaces;
+
+namespace FreeCourse.Web.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CatalogService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<bool> CreateCourseAsync(CourseCreateInput course)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCourseAsync(string courseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CategoryViewModel>> GetAllCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CourseViewModel>> GetAllCoursesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CourseViewModel>> GetAllCoursesByUserIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CourseViewModel> GetCourseById(string courseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateCourseAsync(CourseUpdateInput course)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Frontends/FreeCourse.Web/Services/Interfaces/ICatalogService.cs
+++ Frontends/FreeCourse.Web/Services/Interfaces/ICatalogService.cs
@@ -8,6 +8,7 @@ namespace FreeCourse.Web.Services.Interfaces
     public interface ICatalogService
     {
         Task<List<CourseViewModel>> GetAllCoursesAsync();
+        Task<List<CategoryViewModel>> GetAllCategoriesAsync();
         Task<List<CourseViewModel>> GetAllCoursesByUserIdAsync(string userId);
         Task<CourseViewModel> GetCourseById(string courseId);
         Task<bool> CreateCourseAsync(CourseCreateInput course);
