ad9e9e7a4cb543d237cacf89e3ff46caeb59ccac
Ali Bulut <ali@alibulut.net>
2021-08-14 19:19:44 +0300

    adding Create view for courses

--- Frontends/FreeCourse.Web/Controllers/CourseController.cs
+++ Frontends/FreeCourse.Web/Controllers/CourseController.cs
@@ -6,6 +6,7 @@ using FreeCourse.Shared.Services;
 using FreeCourse.Web.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FreeCourse.Web.Controllers
 {
@@ -24,6 +25,14 @@ namespace FreeCourse.Web.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _catalogService.GetAllCoursesByUserIdAsync(_sharedIdentityService.GetUserId));
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var categories = await _catalogService.GetAllCategoriesAsync();
+            ViewBag.categoryList = new SelectList(categories, "id", "Name");
+
+            return View();
         }
     }
 }
