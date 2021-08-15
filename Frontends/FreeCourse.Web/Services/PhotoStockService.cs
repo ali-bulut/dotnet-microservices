b280b11e44474326e9d0cbdee6ed7501102097d0
Ali Bulut <ali@alibulut.net>
2021-08-15 14:42:36 +0300

    PhotoStockService implementation 2

--- Frontends/FreeCourse.Web/Services/PhotoStockService.cs
+++ Frontends/FreeCourse.Web/Services/PhotoStockService.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using FreeCourse.Web.Models.PhotoStock;
 using FreeCourse.Web.Services.Interfaces;
@@ -8,18 +11,43 @@ namespace FreeCourse.Web.Services
 {
     public class PhotoStockService : IPhotoStockService
     {
-        public PhotoStockService()
+        private readonly HttpClient _httpClient;
+
+        public PhotoStockService(HttpClient httpClient)
         {
+            _httpClient = httpClient;
         }
 
-        public Task<bool> DeletePhoto(string photoUrl)
+        public async Task<bool> DeletePhoto(string photoUrl)
         {
-            throw new NotImplementedException();
+            var response = await _httpClient.DeleteAsync($"photos?photoUrl={photoUrl}");
+            return response.IsSuccessStatusCode;
         }
 
-        public Task<PhotoViewModel> UploadPhoto(IFormFile photo)
+        public async Task<PhotoViewModel> UploadPhoto(IFormFile photo)
         {
-            throw new NotImplementedException();
+            if(photo == null || photo.Length <= 0)
+            {
+                return null;
+            }
+
+            var randomFileName = $"{Guid.NewGuid()}{Path.GetExtension(photo.FileName)}";
+
+            using var ms = new MemoryStream();
+
+            await photo.CopyToAsync(ms);
+
+            var multipartContent = new MultipartFormDataContent();
+            multipartContent.Add(new ByteArrayContent(ms.ToArray()), "photo", randomFileName);
+
+            var response = await _httpClient.PostAsync("photos", multipartContent);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            return await response.Content.ReadFromJsonAsync<PhotoViewModel>();
         }
     }
 }
