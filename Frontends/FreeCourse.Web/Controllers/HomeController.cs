6b0c7d9be5b39f6cc8d4c52a7d27644e1dcae20a
Ali Bulut <ali@alibulut.net>
2021-08-15 15:44:25 +0300

    handling UnauthorizedException

--- Frontends/FreeCourse.Web/Controllers/HomeController.cs
+++ Frontends/FreeCourse.Web/Controllers/HomeController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using FreeCourse.Web.Models;
 using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Diagnostics;
+using FreeCourse.Web.Exceptions;
 
 namespace FreeCourse.Web.Controllers
 {
@@ -34,6 +36,13 @@ namespace FreeCourse.Web.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            var errorFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            if(errorFeature != null && errorFeature.Error is UnauthorizedException)
+            {
+                return RedirectToAction(nameof(AuthController.Logout), "Auth");
+            }
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
