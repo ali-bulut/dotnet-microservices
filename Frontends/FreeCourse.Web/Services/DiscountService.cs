5c392617ccb3709d0139a1aed7ee31002bac257c
Ali Bulut <ali@alibulut.net>
2021-08-16 14:37:26 +0300

    creating IDiscountService and DiscountService

---
+++ Frontends/FreeCourse.Web/Services/DiscountService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using FreeCourse.Web.Models.Discount;
+using FreeCourse.Web.Services.Interfaces;
+
+namespace FreeCourse.Web.Services
+{
+    public class DiscountService : IDiscountService
+    {
+        public Task<DiscountViewModel> GetDiscount(string discountCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
