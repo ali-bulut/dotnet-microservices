235b472b6b2b830ad587556a8fdb5ff23b1eb121
Ali Bulut <ali@alibulut.net>
2021-08-16 16:33:00 +0300

    creating FakePaymentInfo models

--- Services/FakePayment/FreeCourse.Services.FakePayment/Controllers/FakePaymentsController.cs
+++ Services/FakePayment/FreeCourse.Services.FakePayment/Controllers/FakePaymentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FreeCourse.Services.FakePayment.Models;
 using FreeCourse.Shared.ControllerBase;
 using FreeCourse.Shared.Dtos;
 using Microsoft.AspNetCore.Mvc;
@@ -12,7 +13,7 @@ namespace FreeCourse.Services.FakePayment.Controllers
     public class FakePaymentsController : CustomBaseController
     {
        [HttpPost]
-       public IActionResult ReceivePayment()
+       public IActionResult ReceivePayment(FakePaymentInfoDto fakePaymentInfo)
        {
            return CreateActionResultInstance(Response<NoContent>.Success(200));
        }
---
+++ Services/FakePayment/FreeCourse.Services.FakePayment/Models/FakePaymentInfoDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace FreeCourse.Services.FakePayment.Models
+{
+    public class FakePaymentInfoDto
+    {
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
