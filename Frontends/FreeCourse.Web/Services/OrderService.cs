1979b5c246e77f642b170a5d8ab6b2a54807c8f7
Ali Bulut <ali@alibulut.net>
2021-08-16 16:50:59 +0300

    creating IOrderService and OrderService

---
+++ Frontends/FreeCourse.Web/Services/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FreeCourse.Web.Models.Order;
+using FreeCourse.Web.Services.Interfaces;
+
+namespace FreeCourse.Web.Services
+{
+    public class OrderService : IOrderService
+    {
+        public Task<OrderCreatedStatusViewModel> CreateOrder(CheckoutInfoInput checkoutInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<OrderViewModel>> GetOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SuspendOrder(CheckoutInfoInput checkoutInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
