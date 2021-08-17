09e6a3552e62577090f44f04d1c7b06331afc888
Ali Bulut <ali@alibulut.net>
2021-08-17 15:48:22 +0300

    setup consumer part in Order Microservice for handling commands in queue

--- Services/Order/FreeCourse.Services.Order.API/Startup.cs
+++ Services/Order/FreeCourse.Services.Order.API/Startup.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
+using FreeCourse.Services.Order.Application.Consumers;
 using FreeCourse.Services.Order.Infrastructure;
 using FreeCourse.Shared.Services;
+using MassTransit;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
@@ -32,6 +34,26 @@ namespace FreeCourse.Services.Order.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumer<CreateOrderMessageCommandConsumer>();
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(Configuration["RabbitMQUrl"], "/", host =>
+                    {
+                        host.Username("guest");
+                        host.Password("guest");
+                    });
+
+                    cfg.ReceiveEndpoint("create-order-service", e =>
+                    {
+                        e.ConfigureConsumer<CreateOrderMessageCommandConsumer>(context);
+                    });
+                });
+            });
+            services.AddMassTransitHostedService();
+
             services.AddScoped<ISharedIdentityService, SharedIdentityService>();
 
             services.AddDbContext<OrderDbContext>(opt =>
---
+++ Services/Order/FreeCourse.Services.Order.Application/Consumers/CreateOrderMessageCommandConsumer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using FreeCourse.Services.Order.Infrastructure;
+using FreeCourse.Shared.Messages;
+using MassTransit;
+
+namespace FreeCourse.Services.Order.Application.Consumers
+{
+    public class CreateOrderMessageCommandConsumer : IConsumer<CreateOrderMessageCommand>
+    {
+        private readonly OrderDbContext _dbContext;
+
+        public CreateOrderMessageCommandConsumer(OrderDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Consume(ConsumeContext<CreateOrderMessageCommand> context)
+        {
+            var newAddress = new Domain.OrderAggregate.Address(context.Message.Province, context.Message.District,
+                context.Message.Street, context.Message.ZipCode, context.Message.Line);
+
+            Domain.OrderAggregate.Order order = new Domain.OrderAggregate.Order(context.Message.CustomerId, newAddress);
+
+            context.Message.OrderItems.ForEach(x =>
+            {
+                order.AddOrderItem(x.ProductId, x.ProductName, x.Price, x.PictureUrl);
+            });
+
+            await _dbContext.Orders.AddAsync(order);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
